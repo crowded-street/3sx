d5066270a6b77293a27fb561c3387ffc4689bd6d
Artem Pstygo <apstygo.dev@gmail.com>
2025-10-26 11:52:57 +0300

    Adjust timers

--- src/sf33rd/Source/Game/screen/entry.c
+++ src/sf33rd/Source/Game/screen/entry.c
@@ -1352,8 +1352,8 @@ void Break_Into_02(s16 /* unused */) {
         grade_check_work_1st_init(New_Challenger, 0);
     }
 
-    Select_Timer = 48;
-    Unit_Of_Timer = 50;
+    Select_Timer = 0x30;
+    Unit_Of_Timer = 60;
 }
 
 void Break_Into_04(s16 /* unused */) {
