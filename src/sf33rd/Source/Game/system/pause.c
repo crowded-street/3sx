3e08e5d49c804409da60f3550b944cd7b8ef6cac
Artem Pstygo <apstygo.dev@gmail.com>
2026-03-02 20:27:46 +0300

    Adjust formatting

--- src/sf33rd/Source/Game/system/pause.c
+++ src/sf33rd/Source/Game/system/pause.c
@@ -129,10 +129,9 @@ void dispControllerWasRemovedMessage(s32 x, s32 y, s32 step) {
 
     if (Pause_ID) {
         SSPutStrPro(0, x, (y + (step * 2)), 9, -1, "controller port 2.");
-        return;
+    } else {
+        SSPutStrPro(0, x, (y + (step * 2)), 9, -1, "controller port 1.");
     }
-
-    SSPutStrPro(0, x, (y + (step * 2)), 9, -1, "controller port 1.");
 }
 
 s32 Check_Pause_Term(u16 sw, u8 PL_id) {
