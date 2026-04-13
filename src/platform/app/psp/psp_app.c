799fe9b74c06e1f6422e08012d31c61fdd5c86eb
Artem Pstygo <apstygo.dev@gmail.com>
2026-04-13 18:06:05 +0300

    Implement SDL-based input

--- src/platform/app/psp/psp_app.c
+++ src/platform/app/psp/psp_app.c
@@ -2,9 +2,11 @@
 
 #include "main.h"
 #include "platform/video/psp/psp_renderer.h"
+#include "port/input_backend.h"
 #include "port/io/afs.h"
 #include "port/resources.h"
 
+#include <SDL3/SDL.h>
 #include <pspuser.h>
 
 #include <stdbool.h>
@@ -14,10 +16,30 @@ PSP_MAIN_THREAD_ATTR(PSP_THREAD_ATTR_VFPU | PSP_THREAD_ATTR_USER);
 PSP_HEAP_SIZE_KB(-1024);
 PSP_HEAP_THRESHOLD_SIZE_KB(1024);
 
-static void init() {
+static bool init() {
+    if (!SDL_Init(SDL_INIT_GAMEPAD)) {
+        SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
+        return false;
+    }
+
+    InputBackend_Init();
     AFS_Init(Resources_GetAFSPath());
     PSPRenderer_Init();
     Main_Init();
+    return true;
+}
+
+static void poll_sdl_events() {
+    SDL_Event event;
+
+    while (SDL_PollEvent(&event)) {
+        switch (event.type) {
+        case SDL_EVENT_GAMEPAD_ADDED:
+        case SDL_EVENT_GAMEPAD_REMOVED:
+            InputBackend_HandleGamepadDeviceEvent(&event.gdevice);
+            break;
+        }
+    }
 }
 
 static void begin_frame() {
@@ -31,12 +53,14 @@ static void end_frame() {
 }
 
 int main() {
-    init();
+    if (!init()) {
+        return 1;
+    }
 
     while (true) {
+        poll_sdl_events();
         begin_frame();
         Main_StepFrame();
-
         end_frame();
         Main_FinishFrame();
     }
