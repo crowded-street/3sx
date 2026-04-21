826316cc7ec3f6e5a848f919de68041dea86b88b
Artem Pstygo <apstygo.dev@gmail.com>
2026-04-21 23:39:19 +0300

    Extract shadercross stuff out of build-deps.sh

--- build-deps.sh
+++ build-deps.sh
@@ -151,51 +151,6 @@ else
     echo "SDL3 installed to $SDL_BUILD"
 fi
 
-# SDL_shadercross
-# -----------------------------
-
-SDL_SHADERCROSS_REF="main"
-SDL_SHADERCROSS_DIR="$THIRD_PARTY/SDL_shadercross"
-SDL_SHADERCROSS_BUILD="$SDL_SHADERCROSS_DIR/build"
-
-if [ -d "$SDL_SHADERCROSS_BUILD" ]; then
-    echo "SDL_shadercross already built at $SDL_SHADERCROSS_BUILD"
-else
-    echo "Building SDL_shadercross @ $SDL_SHADERCROSS_REF..."
-
-    SDL_SHADERCROSS_SRC=$(mktemp -d)
-    git clone \
-        --branch "$SDL_SHADERCROSS_REF" \
-        --single-branch \
-        --recurse-submodules \
-        https://github.com/libsdl-org/SDL_shadercross.git \
-        "$SDL_SHADERCROSS_SRC"
-
-    cmake -S "$SDL_SHADERCROSS_SRC" -B "$SDL_SHADERCROSS_SRC/cmake-build" \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_INSTALL_PREFIX="$SDL_SHADERCROSS_BUILD" \
-        -DCMAKE_PREFIX_PATH="$SDL_BUILD" \
-        -DSDL3_DIR="$SDL_BUILD/lib/cmake/SDL3" \
-        -DBUILD_SHARED_LIBS=OFF \
-        -DSDLSHADERCROSS_VENDORED=ON \
-        -DSDLSHADERCROSS_SPIRVCROSS_SHARED=OFF \
-        -DSDLSHADERCROSS_SHARED=OFF \
-        -DSDLSHADERCROSS_STATIC=ON \
-        -DSDLSHADERCROSS_CLI=ON \
-        -DSDLSHADERCROSS_CLI_STATIC=ON \
-        -DSDLSHADERCROSS_INSTALL=ON
-
-    cmake --build "$SDL_SHADERCROSS_SRC/cmake-build" --target shadercross -j"$BUILD_JOBS"
-    cmake --install "$SDL_SHADERCROSS_SRC/cmake-build"
-
-    if [ "$OS" = "Darwin" ] && [ -f "$SDL_SHADERCROSS_BUILD/bin/shadercross" ]; then
-        install_name_tool -add_rpath "@executable_path/../lib" "$SDL_SHADERCROSS_BUILD/bin/shadercross" || true
-    fi
-
-    rm -rf "$SDL_SHADERCROSS_SRC"
-    echo "SDL_shadercross CLI installed to $SDL_SHADERCROSS_BUILD"
-fi
-
 # -----------------------------
 # GekkoNet
 # -----------------------------
