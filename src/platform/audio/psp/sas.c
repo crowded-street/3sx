cd44799d848cc62c1570bb0f7cb088a8ce23ee23
Ziemas <ziemas@ziemas.se>
2026-04-26 16:48:08 +0200

    Add error messages, remove reduntant comments

--- src/platform/audio/psp/sas.c
+++ src/platform/audio/psp/sas.c
@@ -23,11 +23,7 @@ static void sas_thread() {
     SDL_SetCurrentThreadPriority(SDL_THREAD_PRIORITY_TIME_CRITICAL);
     while (1) {
         user_cb();
-
-        /* Run a SAS cycle, which will update and process the voices. */
         __sceSasCore(&core, mixer);
-
-        /* Finally, output the audio. */
         sceAudioOutputPannedBlocking(channel, PSP_AUDIO_VOLUME_MAX, PSP_AUDIO_VOLUME_MAX, mixer);
     }
 }
@@ -36,26 +32,24 @@ void SPU_Init(void (*cb)()) {
     soundLock = SDL_CreateMutex();
     user_cb = cb;
 
-    /* The following modules must be loaded in order for sceSasCore to work */
     int result = sceUtilityLoadModule(PSP_MODULE_AV_AVCODEC);
     if (result < 0) {
-        SDL_Log("err: 0x%08X\n", result);
+        SDL_Log("Failed to load avcodec (err: 0x%08X)\n", result);
     }
 
     result = sceUtilityLoadModule(PSP_MODULE_AV_SASCORE);
     if (result < 0) {
-        SDL_Log("err: 0x%08X\n", result);
+        SDL_Log("Failed to load sascore (err: 0x%08X)\n", result);
     }
 
-    /* Initialize sceSasCore instance */
     result = __sceSasInit(&core, GRAIN_SIZE, SPU_VOICE_COUNT, PSP_SAS_OUTPUTMODE_STEREO, PSP_SAS_SAMPLE_RATE);
     if (result < 0) {
-        SDL_Log("err: 0x%08X\n", result);
+        SDL_Log("Failed to initialize sas (err: 0x%08X)\n", result);
     }
 
     channel = sceAudioChReserve(PSP_AUDIO_NEXT_CHANNEL, GRAIN_SIZE, PSP_AUDIO_FORMAT_STEREO);
     if (channel < 0) {
-        SDL_Log("Failed to initialize sceAudio channel!. err: 0x%08X\n", channel);
+        SDL_Log("Failed to initialize sceAudio channel! (err: 0x%08X)\n", channel);
     }
 
     SDL_CreateThread(sas_thread, "sas_thread", NULL);
