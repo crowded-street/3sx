062eb18670055edec4ae388d02fefe1e23ada5e0
Artem Pstygo <apstygo.dev@gmail.com>
2026-04-26 15:32:57 +0300

    Fix glitchy zoomed in sprites

--- src/sf33rd/Source/Game/rendering/mtrans.c
+++ src/sf33rd/Source/Game/rendering/mtrans.c
@@ -1616,9 +1616,6 @@ s32 seqsStoreChip(f32 x, f32 y, s32 w, s32 h, s32 gix, s32 code, s32 attr, s32 a
     s32 u;
     s32 v;
 
-    const f32 dx = 0;
-    const f32 dy = 0;
-
     chip = &seqs_w.chip[seqs_w.sprTotal];
     chip->v[0].x = x;
     chip->v[0].y = y;
@@ -1644,20 +1641,27 @@ s32 seqsStoreChip(f32 x, f32 y, s32 w, s32 h, s32 gix, s32 code, s32 attr, s32 a
 
     appRenewTempPriority_1_Chip();
 
+    const f32 screen_w = SDL_fabsf(chip->v[1].x - chip->v[0].x);
+    const f32 screen_h = SDL_fabsf(chip->v[0].y - chip->v[1].y);
+    const bool scaled = SDL_fabsf(screen_w - (f32)w) > 0.001f || SDL_fabsf(screen_h - (f32)h) > 0.001f;
+
+    const f32 uv_dx = scaled ? 0.5f : 0.0f;
+    const f32 uv_dy = scaled ? 0.5f : 0.0f;
+
     if (attr & 0x8000) {
-        chip->t[1].s = (u - dx) / 256.0f;
-        chip->t[0].s = (u + w - dx) / 256.0f;
+        chip->t[1].s = (u + uv_dx) / 256.0f;
+        chip->t[0].s = (u + w - uv_dx) / 256.0f;
     } else {
-        chip->t[0].s = (u + dx) / 256.0f;
-        chip->t[1].s = (u + w + dx) / 256.0f;
+        chip->t[0].s = (u + uv_dx) / 256.0f;
+        chip->t[1].s = (u + w - uv_dx) / 256.0f;
     }
 
     if (attr & 0x4000) {
-        chip->t[1].t = (v - dy) / 256.0f;
-        chip->t[0].t = (v + h - dy) / 256.0f;
+        chip->t[1].t = (v + uv_dy) / 256.0f;
+        chip->t[0].t = (v + h - uv_dy) / 256.0f;
     } else {
-        chip->t[0].t = (v + dy) / 256.0f;
-        chip->t[1].t = (v + h + dy) / 256.0f;
+        chip->t[0].t = (v + uv_dy) / 256.0f;
+        chip->t[1].t = (v + h - uv_dy) / 256.0f;
     }
 
     chip->tex_code |= ppgGetUsingPaletteHandle(NULL, attr & 0x1FF) << 16;
