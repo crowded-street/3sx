283e7e30dd8cda9fcf07c1ea54ca6d3b8509610f
Artem Pstygo <apstygo.dev@gmail.com>
2026-05-19 23:54:54 +0300

    Implement hold to pause in VS mode

--- src/sf33rd/Source/Game/system/pause.c
+++ src/sf33rd/Source/Game/system/pause.c
@@ -17,7 +17,11 @@
 #include "sf33rd/Source/Game/system/work_sys.h"
 #include "sf33rd/Source/Game/ui/sc_sub.h"
 
+#define HOLD_TO_PAUSE_TIMER_MAX 20
+
 u8 PAUSE_X;
+
+static u8 hold_to_pause_timers[2] = { 0 };
 
 void Pause_Task(struct _TASK* task_ptr);
 
@@ -34,7 +38,7 @@ void Flash_Pause_2nd(struct _TASK* task_ptr);
 void Flash_Pause_3rd(struct _TASK* /* unused */);
 void Flash_Pause_4th(struct _TASK* task_ptr);
 
-s32 Check_Pause_Term(u16 sw, u8 PL_id);
+s32 Check_Pause_Term(u8 PL_id, bool ignore_input);
 void Exit_Pause(struct _TASK* task_ptr);
 void Setup_Pause(struct _TASK* task_ptr);
 void Setup_Come_Out(struct _TASK* task_ptr);
@@ -53,8 +57,8 @@ void Pause_Task(struct _TASK* task_ptr) {
 void Pause_Check(struct _TASK* task_ptr) {
     PAUSE_X = 0;
 
-    if (Check_Pause_Term(~PLsw[0][1] & PLsw[0][0], 0) == 0) {
-        Check_Pause_Term(~PLsw[1][1] & PLsw[1][0], 1);
+    if (Check_Pause_Term(0, false) == 0) {
+        Check_Pause_Term(1, false);
     }
 
     switch (PAUSE_X) {
@@ -74,20 +78,27 @@ void Pause_Move(struct _TASK* task_ptr) {
     }
 }
 
-void Pause_Sleep(struct _TASK* /* unused */) {};
+void Pause_Sleep(struct _TASK* /* unused */) {
+    // Do nothing
+}
 
-void Pause_Die(struct _TASK* /* unused */) {};
+void Pause_Die(struct _TASK* /* unused */) {
+    // Do nothing
+}
 
 void Flash_Pause(struct _TASK* task_ptr) {
-    void (*Flash_Jmp_Tbl[5])(
-        struct _TASK*) = { Flash_Pause_Sleep, Flash_Pause_1st, Flash_Pause_2nd, Flash_Pause_3rd, Flash_Pause_4th };
+    void (*Flash_Jmp_Tbl[5])(struct _TASK*) = {
+        Flash_Pause_Sleep, Flash_Pause_1st, Flash_Pause_2nd, Flash_Pause_3rd, Flash_Pause_4th
+    };
 
     if (Pause_Down != 0) {
         Flash_Jmp_Tbl[task_ptr->r_no[2]](task_ptr);
     }
 }
 
-void Flash_Pause_Sleep(struct _TASK* /* unused */) {}
+void Flash_Pause_Sleep(struct _TASK* /* unused */) {
+    // Do nothing
+}
 
 void Flash_Pause_1st(struct _TASK* task_ptr) {
     if (--task_ptr->free[0] == 0) {
@@ -100,22 +111,22 @@ void Flash_Pause_2nd(struct _TASK* task_ptr) {
     if (--task_ptr->free[0]) {
         if (Pause_ID == 0) {
             SSPutStr2(20, 9, 9, "1P PAUSE");
-            return;
+        } else {
+            SSPutStr2(20, 9, 9, "2P PAUSE");
         }
-
-        SSPutStr2(20, 9, 9, "2P PAUSE");
-        return;
+    } else {
+        task_ptr->r_no[2] = 1;
+        task_ptr->free[0] = 30;
     }
-
-    task_ptr->r_no[2] = 1;
-    task_ptr->free[0] = 30;
 }
 
-void Flash_Pause_3rd(struct _TASK* /* unused */) {}
+void Flash_Pause_3rd(struct _TASK* /* unused */) {
+    // Do nothing
+}
 
 void Flash_Pause_4th(struct _TASK* task_ptr) {
     if (Interface_Type[Pause_ID] == 0) {
-        dispControllerWasRemovedMessage(0x84, 0x52, 0x10);
+        dispControllerWasRemovedMessage(132, 82, 16);
         return;
     }
 
@@ -125,16 +136,54 @@ void Flash_Pause_4th(struct _TASK* task_ptr) {
 
 void dispControllerWasRemovedMessage(s32 x, s32 y, s32 step) {
     SSPutStrPro(0, x, y, 9, -1, "Please reconnect");
-    SSPutStrPro(0, x, (y + step), 9, -1, "the controller to");
+    SSPutStrPro(0, x, y + step, 9, -1, "the controller to");
 
     if (Pause_ID) {
-        SSPutStrPro(0, x, (y + (step * 2)), 9, -1, "controller port 2.");
+        SSPutStrPro(0, x, y + step * 2, 9, -1, "controller port 2.");
     } else {
-        SSPutStrPro(0, x, (y + (step * 2)), 9, -1, "controller port 1.");
+        SSPutStrPro(0, x, y + step * 2, 9, -1, "controller port 1.");
     }
 }
 
-s32 Check_Pause_Term(u16 sw, u8 PL_id) {
+static s32 handle_start_button(u8 PL_id, u16 current_sw, u16 edge_sw) {
+    switch (Mode_Type) {
+    case MODE_VERSUS:
+        if ((edge_sw & SWK_START) && (hold_to_pause_timers[PL_id] == 0)) {
+            hold_to_pause_timers[PL_id] = HOLD_TO_PAUSE_TIMER_MAX;
+        } else if ((current_sw & SWK_START) && (hold_to_pause_timers[PL_id] > 0)) {
+            hold_to_pause_timers[PL_id] -= 1;
+
+            if (hold_to_pause_timers[PL_id] == 0) {
+                hold_to_pause_timers[PL_id ^ 1] = 0;
+                Pause_Type = 1;
+                return PAUSE_X = 1;
+            }
+        } else {
+            hold_to_pause_timers[PL_id] = 0;
+        }
+
+        break;
+
+    case MODE_ARCADE:
+    case MODE_NORMAL_TRAINING:
+    case MODE_PARRY_TRAINING:
+    case MODE_REPLAY:
+        if (edge_sw & SWK_START) {
+            Pause_Type = 1;
+            return PAUSE_X = 1;
+        }
+
+        break;
+
+    case MODE_NETWORK:
+        // Do nothing
+        break;
+    }
+
+    return 0;
+}
+
+s32 Check_Pause_Term(u8 PL_id, bool ignore_input) {
     if (Demo_Flag == 0) {
         return 0;
     }
@@ -157,9 +206,18 @@ s32 Check_Pause_Term(u16 sw, u8 PL_id) {
         return 0;
     }
 
-    if (sw & SWK_START) {
-        Pause_Type = 1;
-        return PAUSE_X = 1;
+    u16 current_sw = PLsw[PL_id][0];
+    u16 edge_sw = ~PLsw[PL_id][1] & PLsw[PL_id][0];
+
+    if (ignore_input) {
+        current_sw = 0;
+        edge_sw = 0;
+    }
+
+    const s32 return_code = handle_start_button(PL_id, current_sw, edge_sw);
+
+    if (return_code != 0) {
+        return return_code;
     }
 
     if (Present_Mode == 3) {
@@ -179,7 +237,7 @@ s32 Check_Pause_Term(u16 sw, u8 PL_id) {
 void Exit_Pause(struct _TASK* task_ptr) {
     u8 ix;
 
-    if (Present_Mode != 3 && Check_Pause_Term(0, Pause_ID ^ 1)) {
+    if (Present_Mode != 3 && Check_Pause_Term(Pause_ID ^ 1, true)) {
         Exit_Menu = 0;
         return;
     }
@@ -242,7 +300,7 @@ void Setup_Come_Out(struct _TASK* task_ptr) {
     Exit_Menu = 0;
 
     for (ix = 0; ix < 4; ix++) {
-        Menu_Suicide[(ix)] = 0;
+        Menu_Suicide[ix] = 0;
     }
 
     Order[0x8A] = 3;
@@ -255,7 +313,7 @@ void Setup_Come_Out(struct _TASK* task_ptr) {
 s32 Check_Play_Status(s16 PL_id) {
     if (Mode_Type != MODE_VERSUS) {
         return Round_Operator[PL_id];
+    } else {
+        return 1;
     }
-
-    return 1;
 }
