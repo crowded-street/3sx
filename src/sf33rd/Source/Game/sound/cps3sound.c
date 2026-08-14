08c3a60dfe7c37755838c657b2e04dac85adfedf
Artem Pstygo <apstygo.dev@gmail.com>
2026-08-14 15:19:37 +0300

    Refactor

--- src/sf33rd/Source/Game/sound/cps3sound.c
+++ src/sf33rd/Source/Game/sound/cps3sound.c
@@ -8,14 +8,20 @@
 #define SS_CHANNEL_COUNT 16
 #define SS_CHANNEL_STATE_FREE 0xC0
 #define SS_READ_BE16(data) ((Uint16)(((Uint16)(data)[0] << 8) | (data)[1]))
-#define SS_READ_BE32(data)                                                                                           \
+#define SS_READ_BE32(data)                                                                                             \
     (((Uint32)(data)[0] << 24) | ((Uint32)(data)[1] << 16) | ((Uint32)(data)[2] << 8) | (data)[3])
 
+typedef struct SsSampleHeader {
+    Uint32 start;
+    Uint32 loop_offset;
+    Uint32 end_offset;
+    Uint32 key;
+} SsSampleHeader;
 typedef struct {
     const Uint8* stream;
     const Uint8* track;
-    const Uint8* unk_08;
-    const Uint8* unk_0C;
+    const Uint8* instrument;
+    const SsSampleHeader* sample_header;
     Uint32 unk_10;
     Uint32 unk_14;
     Uint32 unk_18;
@@ -72,9 +78,16 @@ typedef struct {
     Sint16 control;
 } SsPanDescriptor;
 
+typedef struct SsRequestBank {
+    Uint16 request_count;
+    Uint8 unk_2;
+    Uint8 unk_3;
+    Uint32 request_offsets[];
+} SsRequestBank;
+
 Uint32 ss_unk_02007EE8;                               /* 0x02007EE8 */
-Uint8* ss_unk_02078CFC;                               /* 0x02078CFC */
-Uint8* ss_unk_02078D00;                               /* 0x02078D00 */
+SsSampleHeader* ss_sample_headers;                    /* 0x02078CFC */
+Uint8** ss_instrument_banks;                          /* 0x02078D00 */
 Uint32 ss_unk_02078D04;                               /* 0x02078D04 */
 SsChannelState ss_bgm_channels[SS_CHANNEL_COUNT];     /* 0x02078D0C */
 SsChannelState ss_sfx_channels[SS_CHANNEL_COUNT];     /* 0x0207944C */
@@ -82,65 +95,54 @@ SsPanDescriptor ss_pan_descriptors[SS_CHANNEL_COUNT]; /* 0x02079B8C */
 Uint32 ss_unk_02079C14;                               /* 0x02079C14 */
 Uint32 ss_channel_aux[SS_CHANNEL_COUNT];              /* 0x02079C18 */
 Uint8 ss_unk_02079C58;                                /* 0x02079C58 */
-Uint8* ss_request_data;                               /* 0x02079C6C */
+SsRequestBank* ss_request_data;                       /* 0x02079C6C */
 SsPanDescriptor ss_pan_tmp;                           /* 0x02079C70 */
 Uint16 ss_unk_02079C78;                               /* 0x02079C78 */
 Uint16 ss_request_count;                              /* 0x02079C7A */
 Uint8 ss_state_flags;                                 /* 0x02079C8C */
 
-void SsRequestCore(Uint16 req_number, Sint16 pan_control);
 void SsResetBgmChannels();
 Sint32 SsReadDelay(const Uint8* stream, Uint32* delay);
 
 /* 0x061394D4 */
 void SsRequestCore(Uint16 req_number, Sint16 pan_control) {
-    const Uint8* request;
-    const Uint8* track;
-    const Uint8* track_offsets;
-    const Uint8* unk_08_ptr;
-    SsChannelState* channel;
-    SsPanDescriptor* pan;
-    Uint32* channel_aux;
-    Uint32 request_offset;
-    Uint32 track_offset;
-    Uint32 delay;
-    Uint8 type;
+    req_number %= ss_request_count;
 
-    while (req_number >= ss_request_count) {
-        req_number -= ss_request_count;
-    }
-
-    request_offset = SS_READ_BE32(ss_request_data + 4 + (req_number * 4));
+    const Uint32 request_offset = SDL_Swap32BE(ss_request_data->request_offsets[req_number]);
 
     if (request_offset == 0) {
         return;
     }
 
-    request = ss_request_data + request_offset;
-    type = request[0];
-    track_offsets = request + 1;
+    const Uint8* request = (Uint8*)ss_request_data + request_offset;
+    const Uint8 type = request[0];
+    const Uint8* track_offsets = request + 1;
 
     if (type == 0) {
         SsResetBgmChannels();
 
         for (int i = 0; i < SDL_arraysize(ss_bgm_channels); i++) {
-            channel = &ss_bgm_channels[i];
-            track_offset = SS_READ_BE16(track_offsets + (i * 2));
+            SsChannelState* channel = &ss_bgm_channels[i];
+            const Uint32 track_offset = SS_READ_BE16(track_offsets + i * 2);
 
             if (track_offset == 0) {
                 channel->state = SS_CHANNEL_STATE_FREE;
                 continue;
             }
 
-            track = request + track_offset;
+            const Uint8* track = request + track_offset;
             channel->track = track;
+
+            Uint32 delay;
             channel->stream = track + SsReadDelay(track, &delay);
             channel->delay = delay << 8;
             channel->state = 0x20;
 
-            unk_08_ptr = ss_unk_02078D00 + SS_READ_BE16(ss_unk_02078D00);
-            channel->unk_08 = unk_08_ptr;
-            channel->unk_0C = ss_unk_02078CFC + (SS_READ_BE16(unk_08_ptr + 4) * 0x10);
+            const Uint8* instrument_bank = ss_instrument_banks[0];
+            const Uint8* instrument = instrument_bank + SS_READ_BE16(instrument_bank);
+            channel->instrument = instrument;
+            channel->sample_header = &ss_sample_headers[SS_READ_BE16(instrument + 4)];
+
             channel->unk_24 = 0;
             channel->unk_10 = 0;
             channel->unk_14 = 0;
@@ -193,25 +195,27 @@ void SsRequestCore(Uint16 req_number, Sint16 pan_control) {
         ss_unk_02079C14 = 0;
         ss_state_flags |= 2;
     } else if ((type & 0x80) == 0) {
-        channel_aux = ss_channel_aux;
-        pan = ss_pan_descriptors;
-
-        for (int i = 0; i < SDL_arraysize(ss_sfx_channels); i++, channel_aux++, pan++) {
-            channel = &ss_sfx_channels[i];
-            track_offset = SS_READ_BE16(track_offsets + (i * 2));
+        for (int i = 0; i < SDL_arraysize(ss_sfx_channels); i++) {
+            SsPanDescriptor* pan = &ss_pan_descriptors[i];
+            SsChannelState* channel = &ss_sfx_channels[i];
+            const Uint32 track_offset = SS_READ_BE16(track_offsets + i * 2);
 
             if ((track_offset == 0) || ((channel->priority > type) && ((channel->state & 0x80) == 0))) {
                 continue;
             }
 
-            track = request + track_offset;
+            const Uint8* track = request + track_offset;
+
+            Uint32 delay;
             channel->stream = track + SsReadDelay(track, &delay);
             channel->delay = delay << 8;
             channel->state = 0;
 
-            unk_08_ptr = ss_unk_02078D00 + SS_READ_BE16(ss_unk_02078D00);
-            channel->unk_08 = unk_08_ptr;
-            channel->unk_0C = ss_unk_02078CFC + (SS_READ_BE16(unk_08_ptr + 4) * 0x10);
+            const Uint8* instrument_bank = ss_instrument_banks[0];
+            const Uint8* instrument = instrument_bank + SS_READ_BE16(instrument_bank);
+            channel->instrument = instrument;
+            channel->sample_header = &ss_sample_headers[SS_READ_BE16(instrument + 4)];
+
             channel->unk_10 = 0;
             channel->unk_14 = 0;
             channel->unk_18 = 0;
@@ -263,20 +267,25 @@ void SsRequestCore(Uint16 req_number, Sint16 pan_control) {
                 *pan = ss_pan_tmp;
             }
 
-            *channel_aux = 0;
+            ss_channel_aux[i] = 0;
         }
     } else {
         const int channel_index = type & 0x0F;
-        channel = &ss_sfx_channels[channel_index];
+        SsChannelState* channel = &ss_sfx_channels[channel_index];
+        // In compact, fixed-channel requests track data follows type
+        const Uint8* track = request + 1;
 
-        if (((channel->priority & 0x7F) > (track_offsets[0] & 0x7F)) && ((channel->state & 0x80) == 0)) {
+        if (((channel->priority & 0x7F) > (track[0] & 0x7F)) && ((channel->state & 0x80) == 0)) {
             return;
         }
 
-        channel->stream = track_offsets;
-        unk_08_ptr = ss_unk_02078D00 + SS_READ_BE16(ss_unk_02078D00);
-        channel->unk_08 = unk_08_ptr;
-        channel->unk_0C = ss_unk_02078CFC + (SS_READ_BE16(unk_08_ptr + 4) * 0x10);
+        channel->stream = track;
+
+        const Uint8* instrument_bank = ss_instrument_banks[0];
+        const Uint8* instrument = instrument_bank + SS_READ_BE16(instrument_bank);
+        channel->instrument = instrument;
+        channel->sample_header = &ss_sample_headers[SS_READ_BE16(instrument + 4)];
+
         channel->unk_42 = 0;
         channel->unk_48 = 0;
         channel->unk_4A = 0;
@@ -292,10 +301,10 @@ void SsRequestCore(Uint16 req_number, Sint16 pan_control) {
         channel->unk_56 = 0;
         channel->unk_5F = 0;
         channel->unk_60 = 0x40;
-        channel->priority = track_offsets[0] | 0x80;
+        channel->priority = track[0] | 0x80;
         channel->unk_6D = 0x40;
 
-        pan = &ss_pan_descriptors[channel_index];
+        SsPanDescriptor* pan = &ss_pan_descriptors[channel_index];
 
         if (pan_control == -1) {
             pan->start = 0;
