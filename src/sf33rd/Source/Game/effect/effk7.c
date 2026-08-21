ef42a519e47ac4bca43d83e2b9b350fd79c367dc
OddBread <AwesomeSauce11@outlook.com>
2026-08-20 22:38:06 -0400

    forgot to remove an include

--- src/sf33rd/Source/Game/effect/effk7.c
+++ src/sf33rd/Source/Game/effect/effk7.c
@@ -12,7 +12,6 @@
 #include "sf33rd/Source/Game/engine/slowf.h"
 #include "sf33rd/Source/Game/engine/workuser.h"
 #include "sf33rd/Source/Game/rendering/meta_col.h"
-#include <stdio.h>
 
 void K7_move_type_0(WORK_Other* ewk, PLW* mwk);
 s16 K7_mt0_rebirth_check(PLW* mwk);
