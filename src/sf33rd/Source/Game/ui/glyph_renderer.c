564e42649bf23b1c5a4039bb513ed9e717a3c57a
Artem Pstygo <apstygo.dev@gmail.com>
2026-09-11 23:04:52 +0700

    Simplify glyph renderer (#384)

--- src/sf33rd/Source/Game/ui/glyph_renderer.c
+++ src/sf33rd/Source/Game/ui/glyph_renderer.c
@@ -8,7 +8,8 @@
 #include <stdarg.h>
 
 #define GLYPH_COLOR_STRIDE 4
-#define GLYPH_SIZE 8
+
+GlyphRendererState glyph_renderer_state = { 0 };
 
 static const char punc_map[] = ".,!:()";
 static const GlyphPosition punc_positions[] = { (GlyphPosition) { 26, 1 }, (GlyphPosition) { 27, 1 },
@@ -45,56 +46,59 @@ bool GlyphRenderer_Init() {
     return glyph_texture != 0;
 }
 
-void GlyphRenderer_DrawGlyph(GlyphPosition atlas_pos, GlyphPosition screen_pos, GlyphColor color, float z) {
-    atlas_pos.y += GLYPH_COLOR_STRIDE * color;
+void GlyphRenderer_DrawGlyph(GlyphPosition atlas_pos) {
+    atlas_pos.y += GLYPH_COLOR_STRIDE * glyph_renderer_state.color;
 
     FLTexture* texture = get_texture();
     Sprite sprite = { 0 };
 
-    sprite.v[0].z = sprite.v[3].z = z;
-    sprite.v[0].x = screen_pos.x;
-    sprite.v[0].y = screen_pos.y;
-    sprite.v[3].x = screen_pos.x + 8;
-    sprite.v[3].y = screen_pos.y + 8;
+    sprite.v[0].z = sprite.v[3].z = glyph_renderer_state.z;
+    sprite.v[0].x = glyph_renderer_state.position.x;
+    sprite.v[0].y = glyph_renderer_state.position.y;
+    sprite.v[3].x = glyph_renderer_state.position.x + GLYPH_SIZE;
+    sprite.v[3].y = glyph_renderer_state.position.y + GLYPH_SIZE;
 
-    sprite.t[0].s = atlas_pos.x * 8 / (float)texture->width;
-    sprite.t[0].t = atlas_pos.y * 8 / (float)texture->height;
-    sprite.t[3].s = (atlas_pos.x + 1) * 8 / (float)texture->width;
-    sprite.t[3].t = (atlas_pos.y + 1) * 8 / (float)texture->height;
+    sprite.t[0].s = atlas_pos.x * GLYPH_SIZE / (float)texture->width;
+    sprite.t[0].t = atlas_pos.y * GLYPH_SIZE / (float)texture->height;
+    sprite.t[3].s = (atlas_pos.x + 1) * GLYPH_SIZE / (float)texture->width;
+    sprite.t[3].t = (atlas_pos.y + 1) * GLYPH_SIZE / (float)texture->height;
 
     sprite.tex_code = glyph_texture;
     Renderer_SetTexture(glyph_texture);
     Renderer_DrawSprite(&sprite, 0xFFFFFFFF);
+    glyph_renderer_state.position.x += GLYPH_SIZE;
 }
 
-void GlyphRenderer_DrawDigit(Uint8 digit, GlyphPosition screen_pos, GlyphColor color, float z) {
+void GlyphRenderer_DrawDigit(Uint8 digit) {
     if (digit > 9) {
         SDL_assert(false);
         return;
     }
 
-    GlyphRenderer_DrawGlyph((GlyphPosition) { digit, 2 }, screen_pos, color, z);
+    GlyphRenderer_DrawGlyph((GlyphPosition) { digit, 2 });
 }
 
-void GlyphRenderer_DrawChar(char c, GlyphPosition screen_pos, GlyphColor color, float z) {
+void GlyphRenderer_DrawChar(char c) {
     char* punc_ptr = NULL;
 
     if (SDL_isalpha(c)) {
-        GlyphRenderer_DrawGlyph(alpha_to_position(c), screen_pos, color, z);
+        GlyphRenderer_DrawGlyph(alpha_to_position(c));
     } else if (SDL_isdigit(c)) {
-        GlyphRenderer_DrawDigit(c - '0', screen_pos, color, z);
+        GlyphRenderer_DrawDigit(c - '0');
     } else if ((punc_ptr = strchr(punc_map, c))) {
-        const GlyphPosition pos = punc_positions[punc_ptr - punc_map];
-        GlyphRenderer_DrawGlyph(pos, screen_pos, color, z);
+        GlyphRenderer_DrawGlyph(punc_positions[punc_ptr - punc_map]);
     } else if (SDL_isspace(c)) {
-        // Do nothing
+        glyph_renderer_state.position.x += GLYPH_SIZE;
     } else {
         // Draw an X if the character doesn't exist in the spritesheet
-        GlyphRenderer_DrawGlyph((GlyphPosition) { 23, 0 }, screen_pos, GLYPH_COLOR_HEAVY, z);
+        const GlyphColor old_color = glyph_renderer_state.color;
+        glyph_renderer_state.color = GLYPH_COLOR_HEAVY;
+        GlyphRenderer_DrawGlyph((GlyphPosition) { 23, 0 });
+        glyph_renderer_state.color = old_color;
     }
 }
 
-void GlyphRenderer_DrawString(GlyphPosition screen_pos, GlyphColor color, float z, const char* format, ...) {
+void GlyphRenderer_DrawString(const char* format, ...) {
     char buff[128];
     va_list args;
     va_start(args, format);
@@ -102,7 +106,6 @@ void GlyphRenderer_DrawString(GlyphPosition screen_pos, GlyphColor color, float 
     va_end(args);
 
     for (const char* c = buff; *c != '\0'; c++) {
-        GlyphRenderer_DrawChar(*c, screen_pos, color, z);
-        screen_pos.x += GLYPH_SIZE;
+        GlyphRenderer_DrawChar(*c);
     }
 }
