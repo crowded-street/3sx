3b15f165a0d3336592985be73b6ead844d156f6b
OddBread <58830765+OddBread@users.noreply.github.com>
2026-09-11 11:22:25 -0400

    Add Basic String Rendering Functionality (#373)
    * added basic char rendering function
    
    added basic char rendering function
    
    * added string display support
    
    Currently supports uppercase, lowercase, and spaces. All other characters converted to a red X
    
    * basic punctuation support
    
    * Map additional characters
    
    Glyph sheet doesn't have underscores right now so I have to hackily do it like this. Probably will fix once/if underscores get added.
    
    * render numbers in strings
    
    * applied revisions
    
    * Address review comments
    
    ---------
    
    Co-authored-by: Artem Pstygo <apstygo.dev@gmail.com>

--- src/sf33rd/Source/Game/ui/glyph_renderer.c
+++ src/sf33rd/Source/Game/ui/glyph_renderer.c
@@ -5,12 +5,35 @@
 
 #include <SDL3/SDL.h>
 
+#include <stdarg.h>
+
 #define GLYPH_COLOR_STRIDE 4
+#define GLYPH_SIZE 8
+
+static const char punc_map[] = ".,!:()";
+static const GlyphPosition punc_positions[] = { (GlyphPosition) { 26, 1 }, (GlyphPosition) { 27, 1 },
+                                                (GlyphPosition) { 33, 0 }, (GlyphPosition) { 30, 0 },
+                                                (GlyphPosition) { 26, 0 }, (GlyphPosition) { 27, 0 } };
 
 static Uint32 glyph_texture = 0;
 
 static FLTexture* get_texture() {
     return &flTexture[glyph_texture - 1];
+}
+
+static GlyphPosition alpha_to_position(char c) {
+    int x;
+    int y;
+
+    if (SDL_isupper(c)) {
+        x = c - 'A';
+        y = 0;
+    } else {
+        x = c - 'a';
+        y = 1;
+    }
+
+    return (GlyphPosition) { x, y };
 }
 
 bool GlyphRenderer_Init() {
@@ -51,4 +74,35 @@ void GlyphRenderer_DrawDigit(Uint8 digit, GlyphPosition screen_pos, GlyphColor c
     }
 
     GlyphRenderer_DrawGlyph((GlyphPosition) { digit, 2 }, screen_pos, color, z);
+}
+
+void GlyphRenderer_DrawChar(char c, GlyphPosition screen_pos, GlyphColor color, float z) {
+    char* punc_ptr = NULL;
+
+    if (SDL_isalpha(c)) {
+        GlyphRenderer_DrawGlyph(alpha_to_position(c), screen_pos, color, z);
+    } else if (SDL_isdigit(c)) {
+        GlyphRenderer_DrawDigit(c - '0', screen_pos, color, z);
+    } else if ((punc_ptr = strchr(punc_map, c))) {
+        const GlyphPosition pos = punc_positions[punc_ptr - punc_map];
+        GlyphRenderer_DrawGlyph(pos, screen_pos, color, z);
+    } else if (SDL_isspace(c)) {
+        // Do nothing
+    } else {
+        // Draw an X if the character doesn't exist in the spritesheet
+        GlyphRenderer_DrawGlyph((GlyphPosition) { 23, 0 }, screen_pos, GLYPH_COLOR_HEAVY, z);
+    }
+}
+
+void GlyphRenderer_DrawString(GlyphPosition screen_pos, GlyphColor color, float z, const char* format, ...) {
+    char buff[128];
+    va_list args;
+    va_start(args, format);
+    SDL_vsnprintf(buff, sizeof(buff), format, args);
+    va_end(args);
+
+    for (const char* c = buff; *c != '\0'; c++) {
+        GlyphRenderer_DrawChar(*c, screen_pos, color, z);
+        screen_pos.x += GLYPH_SIZE;
+    }
 }
