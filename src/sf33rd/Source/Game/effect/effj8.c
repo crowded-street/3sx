9c7dc553e3fe29fd7274c5899b7e2e8d9f260087
Markus Borg <markus.borg@gmail.com>
2026-09-13 09:41:05 +0200

    refactor(effj8): isolate active effect updates

--- src/sf33rd/Source/Game/effect/effj8.c
+++ src/sf33rd/Source/Game/effect/effj8.c
@@ -21,6 +21,20 @@ static s32 can_update_effect(void) {
     return !EXE_flag && !Game_pause && !EXE_obroll;
 }
 
+static void update_dragonfly_effect(WORK_Other* ewk) {
+    if (compel_dead_check(ewk)) {
+        ewk->wu.routine_no[0]++;
+        ewk->wu.disp_flag = 0;
+        return;
+    }
+
+    if (can_update_effect()) {
+        dragonfly_move(ewk);
+    }
+
+    disp_pos_trans_entry(ewk);
+}
+
 void effect_J8_move(WORK_Other* ewk) {
     if (obr_no_disp_check()) {
         return;
@@ -34,17 +48,7 @@ void effect_J8_move(WORK_Other* ewk) {
         break;
 
     case 1:
-        if (compel_dead_check(ewk)) {
-            ewk->wu.routine_no[0]++;
-            ewk->wu.disp_flag = 0;
-            break;
-        }
-
-        if (can_update_effect()) {
-            dragonfly_move(ewk);
-        }
-
-        disp_pos_trans_entry(ewk);
+        update_dragonfly_effect(ewk);
         break;
 
     default:
