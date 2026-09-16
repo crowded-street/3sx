17e1b4f4aa1cdc09378ab06b5abf401425a04bb5
Markus Borg <markus.borg@gmail.com>
2026-09-16 12:32:53 +0200

    refactor(bg): extract ending tile drawing
    Recipe E. Code Health remains 5.68; paired ending-G/C extraction removes the remaining nested tile loops from scr_trans and reduces it to cc 21. Playtest ending tile replacement for G, C, and nosekae states.

--- src/sf33rd/Source/Game/stage/bg.c
+++ src/sf33rd/Source/Game/stage/bg.c
@@ -1106,14 +1106,68 @@ static void draw_stage19_tiles(u8 bgnm, s32 xx[2], s32 yy[2], s32 global_index, 
     }
 }
 
+static void draw_ending_g_tiles(u8 bgnm, s32 xx[2], s32 yy[2], s32 global_index, s32 palOffset,
+                                PPGDataList* curDataList) {
+    s32 x;
+    s32 y;
+    s32 global_index_real;
+
+    for (y = yy[0]; y < yy[1]; y += 128) {
+        for (x = xx[0]; x < xx[1]; x += 128) {
+            global_index_real = global_index + (((y >> 7) << 3) + (x >> 7));
+
+            if (nosekae != 0) {
+                global_index_real = remap_ending_nosekae_chip(global_index_real);
+            }
+
+            if (bgnm == 0) {
+                if (g_kakikae[0]) {
+                    global_index_real = remap_ending_g_kakikae0_chip(global_index_real);
+                }
+
+                if (g_kakikae[1]) {
+                    global_index_real = remap_ending_g_kakikae1_chip(global_index_real);
+                }
+            }
+
+            bgDrawOneChip(x, y, 128, 128, global_index_real, -1, palOffset);
+            ppgSetupCurrentDataList(curDataList);
+        }
+    }
+}
+
+static void draw_ending_c_tiles(u8 bgnm, s32 xx[2], s32 yy[2], s32 global_index, s32 palOffset,
+                                PPGDataList* curDataList) {
+    s32 x;
+    s32 y;
+    s32 global_index_real;
+
+    for (y = yy[0]; y < yy[1]; y += 128) {
+        for (x = xx[0]; x < xx[1]; x += 128) {
+            global_index_real = global_index + (((y >> 7) << 3) + (x >> 7));
+
+            if (bgnm == 0) {
+                switch (c_kakikae) {
+                case 1:
+                    global_index_real = remap_ending_c_kakikae1_chip(global_index_real);
+                    break;
+
+                case 2:
+                    global_index_real = remap_ending_c_kakikae2_chip(global_index_real);
+                }
+            }
+
+            bgDrawOneChip(x, y, 128, 128, global_index_real, -1, palOffset);
+            ppgSetupCurrentDataList(curDataList);
+        }
+    }
+}
+
 void scr_trans(u8 bgnm) {
     PPGDataList* curDataList;
     s32 xx[2];
     s32 yy[2];
-    s32 x;
-    s32 y;
     s32 global_index;
-    s32 global_index_real;
     s32 palOffset;
     u32 vtxColor;
 
@@ -1163,52 +1217,13 @@ void scr_trans(u8 bgnm) {
         break;
 
     case 5:
-        for (y = yy[0]; y < yy[1]; y += 128) {
-            for (x = xx[0]; x < xx[1]; x += 128) {
-                global_index_real = global_index + (((y >> 7) << 3) + (x >> 7));
-
-                if (nosekae != 0) {
-                    global_index_real = remap_ending_nosekae_chip(global_index_real);
-                }
-
-                if (bgnm == 0) {
-                    if (g_kakikae[0]) {
-                        global_index_real = remap_ending_g_kakikae0_chip(global_index_real);
-                    }
-
-                    if (g_kakikae[1]) {
-                        global_index_real = remap_ending_g_kakikae1_chip(global_index_real);
-                    }
-                }
-
-                bgDrawOneChip(x, y, 128, 128, global_index_real, -1, palOffset);
-                ppgSetupCurrentDataList(curDataList);
-            }
-        }
+        draw_ending_g_tiles(bgnm, xx, yy, global_index, palOffset, curDataList);
 
         scr_calc2(bgnm);
         break;
 
     case 6:
-        for (y = yy[0]; y < yy[1]; y += 128) {
-            for (x = xx[0]; x < xx[1]; x += 128) {
-                global_index_real = global_index + (((y >> 7) << 3) + (x >> 7));
-
-                if (bgnm == 0) {
-                    switch (c_kakikae) {
-                    case 1:
-                        global_index_real = remap_ending_c_kakikae1_chip(global_index_real);
-                        break;
-
-                    case 2:
-                        global_index_real = remap_ending_c_kakikae2_chip(global_index_real);
-                    }
-                }
-
-                bgDrawOneChip(x, y, 128, 128, global_index_real, -1, palOffset);
-                ppgSetupCurrentDataList(curDataList);
-            }
-        }
+        draw_ending_c_tiles(bgnm, xx, yy, global_index, palOffset, curDataList);
 
         scr_calc2(bgnm);
         break;
